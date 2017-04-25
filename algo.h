c33a4c302073649c0b983745f74d224e5e22c43e
Levon Sargsyan <levons@internal.synopsys.com>
2017-04-25 20:17:15 +0400

    imporved algo with double enegery arguments

--- algo.h
+++ algo.h
@@ -12,20 +12,21 @@ class JSimulateAnnealingImpl {
 	virtual void  revert_change() = 0;
 	virtual int  make_change()   = 0;
     public:  
-	virtual void post_process_time_step()  {}
-	virtual float decrease_temperature(float T0, float T, float time) {
+	virtual void post_process_time_step()  {
+	
+	  
+	}
+	
+      virtual float decrease_temperature(float T0, float T, float time) {
 	    float alpha = 0.9;
-	    //float beta = 10;
-	    //float delta_t = 1.0;
-	    //return T0 - beta*time;
-	    //return T0/(1+alpha*log(1+time));	
-	    //return T0/(1+alpha*time);	
+	    //float delta_t;
 	    return T0*pow(alpha,time);
 	  }
 
-	virtual  float get_transition_probability(int E, float T ) {
-            //std::cout << m_prev_res << "----" << E << " = "<< m_prev_res-E << std::endl;
-	    return exp(-E/T);
+	virtual  float get_transition_probability(int E1, int E2, float T ) {
+            float r = exp((E1-E2)/T);
+            //std::cout << E1 << "----" << E2 << " = "<< E1-E2 << " p= " << r << std::endl;
+	    return r;
 	  }
 
 };
@@ -50,12 +51,14 @@ class JSimulateAnnealing  {
 	    m_impl->post_process_time_step();
 	}
 
-        int try_make_unbeneficial_change(int E, float T) {
-            float p = m_impl->get_transition_probability(E,T);
+        int try_make_unbeneficial_change(int E1, int E2, float T) {
+            assert(E2>E1);
+             
+            float p = m_impl->get_transition_probability(E1,E2,T);
 	    float v = ((double) rand() / (RAND_MAX)) ;
     
 	    assert( p>0 || p<1 );
-  	    assert(E>m_prev_res);
+  	   
 	    //std::cout << " v is " << v << " p is " << p << std::endl;
 		
 	    if( v >= p ) {
@@ -64,7 +67,7 @@ class JSimulateAnnealing  {
 	    
 	    //assert(0);
 	    //std::cout << "accept" << std::endl;
-	    return E;
+	    return E2;
 	}
 
 	
@@ -80,13 +83,12 @@ class JSimulateAnnealing  {
 		  
 		  res = make_change_and_calculate_result();
 		  if ( res > m_prev_res ) 
-                    res = try_make_unbeneficial_change(res,T);
+                    res = try_make_unbeneficial_change(m_prev_res,res,T);
 	      
 		  //std::cout << "PRes:" << m_prev_res << " CRes:" << res << std::endl;
 		
 		  m_prev_res = res;
 		  T = m_impl->decrease_temperature(T0,T,time);
-		  assert(T0>T);
 		  time = time + time_step;
 		  post_process_time_step();
    		  
@@ -98,7 +100,7 @@ class JSimulateAnnealing  {
   
 private:
 	int m_prev_res;
-	int m_last_res;
+	//int m_last_res;
 	
 	float T0;
 	float Tmin;
